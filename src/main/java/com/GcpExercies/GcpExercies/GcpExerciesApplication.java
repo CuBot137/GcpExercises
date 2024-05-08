822d5be3adfb2a81488f94282f3111c888b4478c
conor <conorlynam1234@gmail.com>
2024-05-08 15:50:32 +0100

    Initial commit

---
+++ src/main/java/com/GcpExercies/GcpExercies/GcpExerciesApplication.java
@@ -0,0 +1,21 @@
+package com.GcpExercies.GcpExercies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GcpExerciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpExerciesApplication.class, args);
+	}
+
+	@GetMapping("get")
+	public String handleGetRequest() {
+		return "Hello from Cloud Function! (Responding to a GET request)";
+	}
+
+}
---
+++ src/test/java/com/GcpExercies/GcpExercies/GcpExerciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GcpExercies.GcpExercies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpExerciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
