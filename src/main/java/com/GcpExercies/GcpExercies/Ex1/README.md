bb9a0b79af02d055f45ae49047a79f58ab22925c
conor <conorlynam1234@gmail.com>
2024-05-22 13:49:35 +0100

    ReadMe

---
+++ src/main/java/com/GcpExercies/GcpExercies/Ex1/README.md
@@ -0,0 +1,6 @@
+# GcpExercise 1
+Google Cloud Functions are blocks of code that are designed to serve a specific purpose.
+They are lightweight, serverless and cost-effective.
+
+In this example I created a function that takes in a http request. It will give a response depending on the request type.
+For example, a get request will return "This is a GET response".
--- src/main/java/com/GcpExercies/GcpExercies/GapExerciseApplication.java
+++ src/main/java/com/GcpExercies/GcpExercies/GapExerciseApplication.java
@@ -2,21 +2,16 @@ package com.GcpExercies.GcpExercies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class GcpExerciesApplication {
+public class GapExerciseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GcpExerciesApplication.class, args);
+		SpringApplication.run(GapExerciseApplication.class, args);
 	}
 
-	@GetMapping("get")
-	public String handleGetRequest() {
-		return "Hello from Cloud Function! (Responding to a GET request)";
-	}
-	// test
+
 
 }
